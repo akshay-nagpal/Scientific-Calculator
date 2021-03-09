b6582d35e36f0da0ea1e4b8b5ed42a4752a50a34
akshay-nagpal <akshay.ngpl10@gmail.com>
2021-03-09 14:54:23 +0530

    menu-driven

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -17,45 +17,49 @@ public class main {
         return f;
     }
     public static void main(String[] args) {
-        System.out.println("Welcome!! \nEnter \n 1 for Square root \n 2 for Factorial \n 3 for Natural logarithm \n 4 for Power function \n");
-        int x;
-        Scanner sc=new Scanner(System.in);
-        x=sc.nextInt();
-        if(x==1)
-        {
-            System.out.println("Enter the number \n");
-            double n;
-            n=sc.nextDouble();
-            System.out.println("Output= ");
-            System.out.println(Math.sqrt(n));
-        }
-        if(x==2)
-        {
-            System.out.println("Enter the number \n");
-            int n;
-            n=sc.nextInt();
-            System.out.println("Output= ");
-            System.out.println(factorial(n));
-        }
-        if(x==3)
-        {
-            System.out.println("Enter the number \n");
-            double n;
-            n=sc.nextDouble();
-            System.out.println("Output= ");
-            System.out.println(Math.log(n));
-        }
-        if(x==4)
-        {
-            System.out.println("Enter two numbers in format a^b \n");
-            System.out.println("Enter a:");
-            double a;
-            a=sc.nextDouble();
-            System.out.println("\nEnter b:");
-            double b;
-            b=sc.nextDouble();
-            System.out.println("Output= ");
-            System.out.println(Math.pow(a,b));
+        System.out.println("Welcome!!");
+        while (true) {
+            System.out.println("Enter \n 1 for Square root \n 2 for Factorial \n 3 for Natural logarithm \n 4 for Power function \n 5 for exit \n");
+            int x;
+            Scanner sc = new Scanner(System.in);
+            x = sc.nextInt();
+            if (x == 1) {
+                System.out.println("Enter the number \n");
+                double n;
+                n = sc.nextDouble();
+                System.out.println("Output= ");
+                System.out.println(Math.sqrt(n));
+            }
+            if (x == 2) {
+                System.out.println("Enter the number \n");
+                int n;
+                n = sc.nextInt();
+                System.out.println("Output= ");
+                System.out.println(factorial(n));
+            }
+            if (x == 3) {
+                System.out.println("Enter the number \n");
+                double n;
+                n = sc.nextDouble();
+                System.out.println("Output= ");
+                System.out.println(Math.log(n));
+            }
+            if (x == 4) {
+                System.out.println("Enter two numbers in format a^b \n");
+                System.out.println("Enter a:");
+                double a;
+                a = sc.nextDouble();
+                System.out.println("\nEnter b:");
+                double b;
+                b = sc.nextDouble();
+                System.out.println("Output= ");
+                System.out.println(Math.pow(a, b));
+            }
+            if(x==5)
+            {
+                System.out.println("Thanks\n Bye!! Have a good day!! \n");
+                break;
+            }
         }
     }
 }
