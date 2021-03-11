a14e1e4a7978859f5f6cd834c49323fe10a3b197
akshay-nagpal <akshay.ngpl10@gmail.com>
2021-03-11 14:49:50 +0530

    junit test cases added

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,32 +4,32 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class main {
-    static BigInteger factorial(int N)
+    public int factorial(int N)
     {
         // Initialize result
-        BigInteger f
-                = new BigInteger("1"); // Or BigInteger.ONE
+        int f=1;
 
         // Multiply f with 2, 3, ...N
         for (int i = 2; i <= N; i++)
-            f = f.multiply(BigInteger.valueOf(i));
+            f = f*i;
 
         return f;
     }
-    static double squareroot(double n)
+     public double squareroot(double n)
     {
         return Math.sqrt(n);
     }
-    static double logarithm(double n)
+     public double logarithm(double n)
     {
         return Math.log(n);
     }
-    static double power(double a,double b)
+    public double power(double a,double b)
     {
         return Math.pow(a,b);
     }
     public static void main(String[] args) {
         System.out.println("Welcome!!");
+        main obj=new main();
         while (true) {
             System.out.println("Enter \n 1 for Square root \n 2 for Factorial \n 3 for Natural logarithm \n 4 for Power function \n 5 for exit \n");
             int x;
@@ -40,21 +40,21 @@ public class main {
                 double n;
                 n = sc.nextDouble();
                 System.out.println("Output= ");
-                System.out.println(squareroot(n));
+                System.out.println(obj.squareroot(n));
             }
             if (x == 2) {
                 System.out.println("Enter the number \n");
                 int n;
                 n = sc.nextInt();
                 System.out.println("Output= ");
-                System.out.println(factorial(n));
+                System.out.println(obj.factorial(n));
             }
             if (x == 3) {
                 System.out.println("Enter the number \n");
                 double n;
                 n = sc.nextDouble();
                 System.out.println("Output= ");
-                System.out.println(logarithm(n));
+                System.out.println(obj.logarithm(n));
             }
             if (x == 4) {
                 System.out.println("Enter two numbers in format a^b \n");
@@ -65,7 +65,7 @@ public class main {
                 double b;
                 b = sc.nextDouble();
                 System.out.println("Output= ");
-                System.out.println(power(a,b));
+                System.out.println(obj.power(a,b));
             }
             if(x==5)
             {
--- src/test/java/mainTest.java
+++ src/test/java/mainTest.java
@@ -1,22 +1,59 @@
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.math.BigInteger;
 
+import static org.junit.Assert.*;
 public class mainTest {
 
+    private static final double DELTA = 1e-15;
+    main mainobj = new main();
+
     @Test
-    public void factorial() {
+    public void factorialTruePositive(){
+        assertNotEquals("Finding factorial of a number for False Positive", 720, mainobj.factorial(6), DELTA);
+        assertNotEquals("Finding factorial of a number for False Positive", 6, mainobj.factorial(3), DELTA);
     }
 
     @Test
-    public void squareroot() {
+    public void factorialFalsePositive(){
+        assertNotEquals("Testing factorial for False Positive", 120, mainobj.factorial(6), DELTA);
+        assertNotEquals("Testing factorial for False Positive", 24, mainobj.factorial(3), DELTA);
     }
 
     @Test
-    public void logarithm() {
+    public void powerTruePositive(){
+        assertEquals("Testing power function for True Positive", 243, mainobj.power(3, 5), DELTA);
+        assertEquals("Testing power function for True Positive",125 , mainobj.power(5, 3), DELTA);
     }
 
     @Test
-    public void power() {
+    public void powerFalsePositive(){
+        assertNotEquals("Testing Power function for False Positive", 768, mainobj.power(3, 4), DELTA);
+        assertNotEquals("Testing Power function for False Positive", 7451, mainobj.power(6, 4), DELTA);
     }
+    @Test
+    public void logarithmTruePositive(){
+        assertEquals("Testing natural log for True Positive", 0, mainobj.logarithm(1), DELTA);
+        assertEquals("Testing natural log for True Positive", 0, mainobj.logarithm(1), DELTA);
+    }
+
+    @Test
+    public void logarithmFalsePositive(){
+        assertNotEquals("Testing natural log for False Positive", 784, mainobj.logarithm(65), DELTA);
+        assertNotEquals("Testing natural log for False Positive", 4871.256, mainobj.logarithm(845), DELTA);
+    }
+
+    @Test
+    public void squarerootTruePositive(){
+        assertEquals("Testing square root for True Positive", 4, mainobj.squareroot(16), DELTA);
+        assertEquals("Testing square root for True Positive", 6, mainobj.squareroot(36), DELTA);
+    }
+
+    @Test
+    public void squarerootFalsePositive(){
+        assertNotEquals("Testing square root for False Positive", 4.5, mainobj.squareroot(32), DELTA);
+        assertNotEquals("Testing square root for False Positive", 5.6, mainobj.squareroot(25), DELTA);
+
+    }
+
 }
